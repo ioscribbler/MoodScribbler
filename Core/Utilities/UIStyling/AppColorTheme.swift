167efcbf8f575a448a0ab1b4055c2188e85ef6ee
ioscribbler <ioscribbler@gmail.com>
2025-01-20 10:42:40 +0200

    Added the source code

---
+++ Core/Utilities/UIStyling/AppColorTheme.swift
@@ -0,0 +1,18 @@
+//
+//  AppColorTheme.swift
+//  Mood Scribbler
+//
+//  Created by ioscribbler on 06.11.2024.
+//
+
+import Foundation
+import SwiftUI
+
+enum AppColorTheme {
+    static let backgroundColor: Color = Color("BackgroundColor")
+    static let secondaryTextColor: Color = Color("SecondaryTextColor")
+    static let secondaryBackgroundColor: Color = Color("SecondaryBackgroundColor")
+    static let whiteColor: Color = Color("WhiteColor")
+    static let accentColor: Color = Color("AccentColor")
+    static let errorColor: Color = .red
+}
---
+++ Core/Utilities/UIStyling/CustomFont.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFont.swift
+//  Mood Scribbler
+//
+//  Created by ioscribbler on 31.10.2024.
+//
+
+import Foundation
+import SwiftUI
+
+enum CustomFont {
+    static let title = Font.custom("Poppins-Regular", size: 32)
+    static let subtitle = Font.custom("Poppins-Regular", size: 24)
+    static let body = Font.custom("Poppins-Regular", size: 16)
+    static let callout = Font.custom("Poppins-Regular", size: 14)
+    static let caption = Font.custom("Poppins-Regular", size: 12)
+}
